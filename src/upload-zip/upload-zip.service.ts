303c4150204959ac6058af0d5748c1221f324365
CryptoTabaluga <sven@florence.finance>
2024-08-26 15:46:18 -0500

    Relay Server communication update localhost3

--- src/upload-zip/upload-zip.service.ts
+++ src/upload-zip/upload-zip.service.ts
@@ -122,7 +122,8 @@ export class UploadZipService implements OnModuleInit {
   }
   
   public async isRelayServerUp(): Promise<string> {
-    const relayURL = this.getRelayUrl();
+    //const relayURL = this.getRelayUrl();
+    const relayURL = this.config.get('lto.relay');
     try {
       const isUp:boolean =await this.isRelayUp(relayURL);
       if(isUp) {
